40b65d3f317b615827ee5def89c533bcbaca03e7
Kathryn Hodge <kahodge@vassar.edu>
2014-11-08 20:44:14 -0500

    starting...

---
+++ Adventure/src/adventure/Adventure.java
@@ -0,0 +1,14 @@
+
+package adventure;
+
+/**
+ *
+ * @author kathrynhodge
+ */
+public class Adventure {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
